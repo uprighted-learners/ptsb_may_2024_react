c02d2a1ec41797cdc72d312f11fbd61b133ad155
u4ik <insighteuphoric@gmail.com>
2024-08-08 20:39:34 -0400

    added posts, create/mine + persistent login

--- react_express/src/App.jsx
+++ react_express/src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./App.css";
 
 import Auth from "./components/Auth/Auth";
+import Posts from "./components/Posts/Posts";
 
 function App() {
   const [sessionToken, setSessionToken] = useState("");
+
+  useEffect(() => {
+    let token = localStorage.getItem("MyToken");
+    if (token) {
+      setSessionToken(token);
+    }
+  }, []);
 
   //? Creating a helper function to handle setting up a token in localStorage
   const updateToken = (token) => {
@@ -14,21 +22,33 @@ function App() {
     setSessionToken(token);
   };
 
+  const clearToken = () => {
+    console.log("Token Removed!");
+    setSessionToken("");
+    localStorage.clear();
+  };
+
   return (
     <>
       {/* Check session token, if it is not avail, show user auth. */}
 
-      {
-      !sessionToken ? <Auth updateToken={updateToken} /> : 
+      {!sessionToken ? (
+        <Auth updateToken={updateToken} />
+      ) : (
         <div>
-            {/* Logout button */}
+          {/* Logout button */}
+          <button
+            style={{ position: "absolute", top: 0, right: 0, margin: "1em" }}
+            onClick={() => clearToken()}
+          >
+            Logout
+          </button>
 
-            {/* Posts component, which will render out all posts, and allow the user to create one */}
-          
-          Posts
-          
+          {/* Posts component, which will render out all posts, and allow the user to create one */}
+
+          <Posts />
         </div>
-      }
+      )}
     </>
   );
 }
---
+++ react_express/src/components/Posts/Posts.jsx
@@ -0,0 +1,84 @@
+import { useState, useEffect } from "react";
+
+const Posts = () => {
+  const [text, setText] = useState("");
+  const [results, setResults] = useState([]);
+
+  const getPosts = async () => {
+    try {
+      let result = await fetch("http://localhost:9000/post/mine", {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("MyToken"),
+        },
+      });
+
+      let json = await result.json();
+
+      setResults(json.Results);
+      console.log(json);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  // Need to do a fetch get all posts
+  // Have to pass an auth token
+  // Display them
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const result = await fetch("http://localhost:9000/post/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // Since this route is protected via Validate.js (in the server), we have to pass the auth token to this request
+          //! Will still cause error, come back to this
+          Authorization: `Bearer ${localStorage.getItem("MyToken")}`,
+        },
+        body: JSON.stringify({
+          text: text,
+        }),
+      });
+
+      const json = await result.json();
+
+      console.log(json);
+      getPosts();
+      setText("")
+
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const displayPosts = () => {
+    return results.map((post) => (
+      <div
+        style={{ border: ".3em solid black", padding: "2em" }}
+        key={post._id}
+      >
+        <h2>{post.text}</h2>
+      </div>
+    ));
+  };
+
+  return (
+    <div>
+      <h3>Create a post:</h3>
+      <form onSubmit={handleSubmit}>
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+
+        <button>Submit</button>
+      </form>
+        {displayPosts()}
+
+    </div>
+  );
+};
+
+export default Posts;
